edeb865f0445f64a3c06f4b4e813e0e51db7b39f
pereiranoemi007-art <pereiranoemi007@gmail.com>
2026-03-27 20:39:54 -0300

    Preparar proyecto para subida: .gitignore y correcciones

--- correxion.py
+++
@@ -1,6 +0,0 @@
-class Database:
-    def __init__(self, filename='coen.db'):
-        self.conn = sqlite3.connect(filename)
-        self.conn.row_factory = sqlite3.Row
-        self.cursor = self.conn.cursor()
-        self.create_tables()  # 👈 se ejecuta solo
